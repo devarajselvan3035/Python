4ca4885bfa50feec4159df2f12277a2c31bfc9dd
devarajselvan3035 <devarajselvan963@gmail>
2026-03-29 12:29:21 +0530

    29-03-2026

--- leetcode/Hash_Table/819_Most_common_word.py
+++ leetcode/Hash_Table/819_Most_common_word.py
@@ -8,8 +8,37 @@ The words in paragraph are case-insentive and the answer should be returned in l
 Node: that words can not contain punctuation sysmbols.
 """
 
+from re import split
 from typing import List
 
 
 def mostCommonWord(paragraph: str, banned: List[str]) -> str:
-    pass
+    l, r = 0, 0
+    flag = 0
+    yes = False
+    res = ""
+    maxcount = 0
+    count = {}
+    while r < len(paragraph):
+        if not paragraph[r].isalpha():
+            yes = True
+            if flag == 0:
+                lowstr = paragraph[l:r].lower()
+                count[lowstr] = count.setdefault(lowstr, 0) + 1
+                if count[lowstr] > maxcount and lowstr not in banned:
+                    res = lowstr
+                    maxcount = count[lowstr]
+            flag = 1
+            r += 1
+            l = r
+        else:
+            flag = 0
+            r += 1
+
+    return res if yes else paragraph
+
+
+paragraph, banned = "Bob hit a ball, the hit BALL flew far after it was hit.", ["hit"]
+# paragraph, banned = "a.", []
+# paragraph, banned = "Bob", []
+print(mostCommonWord(paragraph, banned))
